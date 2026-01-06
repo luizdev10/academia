f714d5ec144676fadf5a6151ce02ed932edc90a0
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-01-06 16:42:47 -0300

    inicializando o git

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,94 @@
+import "./style.css";
+import minhaImg from "../../assets/img/logo.png";
+import foto1 from "../../assets/img/foto1.png";
+//import foto2 from "../../assets/img/logo.png";
+//import foto3 from "../../assets/img/logo.png";
+import { useState } from "react";
+
+function Home() {
+    const fotos = [foto1, minhaImg];
+
+    const [indice, setIndice] = useState(0);
+
+    const proximo = () => {
+        setIndice((prev) => (prev === fotos.length - 1 ? 0 : prev + 1));
+    };
+    const anterior = () => {
+        setIndice((prev) => (prev === 0 ? fotos.length - 1 : prev - 1));
+    };
+    return (
+        <div id="section-body">
+            <nav>
+                <div id="area-logo">
+
+                    <p>Saulo Fitness</p>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="">INICIO</a></li>
+                        <li><a href="">CONTATO</a></li>
+                        <li><a href="">PLANOS</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div id="section-inicio">
+                <h1>PROPORCIONANDO SAÚDE E BEM ESTAR</h1>
+            </div>
+            <div id="modalidades">
+                <div id="titulo-model">
+                    <h2>Nossas modalidades</h2>
+                    <p>participe de nossas aulas</p>
+                    <p>Animadas, eletrizantes e divertidas. É assim que nossas aulas são conhecidas. São muito mais do que músicas agitadas ou movimentos coreografados. É onde você constrói camaradagem e novas amizades com pessoas que têm os mesmos objetivos, paixões e motivações que você. Faça uma aula hoje mesmo e comece a movimentar-se.</p>
+
+                </div>
+                <div id="area-carrosel-model">
+
+                    <button onClick={anterior} className="btn-left-home">❮</button>
+
+
+                    <img
+                        src={fotos[indice]}
+                        alt="Modalidade"
+                        style={{
+                            width: '100%',
+                            height: '450px',
+                            objectFit: 'cover',
+                            borderRadius: '15px'
+                        }}
+                    />
+
+                    <button onClick={proximo} className="btn-rigth-home">❯</button>
+                </div>
+            </div>
+            <div className="sobre-container">
+                <div className="sobre-imagens">
+                    <img src={minhaImg} alt="Treino 1" className="img-menor" />
+                    <img src={foto1} alt="Treino 2" className="img-maior" />
+                </div>
+
+
+                <div className="sobre-texto">
+                    <h2>SOBRE ELE</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste cumque repudiandae, consequatur commodi amet labore illum perferendis dicta, corporis vero delectus sed? Quasi nam aliquid necessitatibus, facere eos ab cum.</p>
+                </div>
+            </div>
+
+            <div id="section-home-servicos">
+                <div id="musculacao-home">
+                        <h2>MUSCULAÇÃO</h2>
+                        <p>Force seus limites e construa o corpo dos seus sonhos.</p>
+                </div>
+                <div id="perda-peso-home">
+                        <h2>PERDA DE PESO</h2>
+                        <p>Queime calorias, acelere seu metabolismo e conquiste o peso ideal.</p>
+                </div>
+                <div id="rebilitacao-home">
+                        <h2>REABILITAÇÃO</h2>
+                        <p>Recupere-se com segurança e volte mais forte</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
