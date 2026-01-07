f648bddcc88981a8c5fcb2db5e0077fa240343c9
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-01-07 11:02:58 -0300

    teste cell2

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
   </StrictMode>,
+
+<React.StrictMode>
+    <BrowserRouter basename="/academia">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
