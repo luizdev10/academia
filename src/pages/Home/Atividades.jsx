78abe323045311c33f0645e6ba959fe89f953cbe
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-01-13 14:04:37 -0300

    reajuste da section de seriços

--- src/pages/Home/Atividades.jsx
+++ src/pages/Home/Atividades.jsx
@@ -4,17 +4,20 @@ import reabili from "../../assets/img/reabili.png"
 function Services() {
     return (
         <section className="bg-amber-100 h-screen flex flex-col justify-center items-center">
-            <div className=" h-52 mt-5 flex flex-col items-center">
+            <div className=" h-52 flex flex-col items-center">
+                <p>MUSCULAÇÃO</p>
                 <img src={musc} alt="musculação" className="h-40 rounded-2xl border-orange-400 border-b-8 border-r-6 shadow-xl " />
-                <p className="w-50 text-center mt-1">Supere seus limites com treinos de força de alta performance</p>
+                <p className="w-60 text-center mt-1">Supere seus limites com treinos de força de alta performance</p>
             </div>
-            <div className=" h-52 mt-7 w-40 flex flex-col items-center">
+            <div className=" h-52 w-40 flex flex-col items-center">
+                <p className="mt-10">PERCA DE PESO</p>
                 <img src={peso} alt="musculação" className="h-40 rounded-2xl border-orange-400 border-b-8 border-r-6 shadow-xl " />
-                <p className="w-50 text-center mt-1">Supere seus limites com treinos de força de alta performance</p>
+                <p className="w-60 text-center mt-2">Supere seus limites com treinos de força de alta performance</p>
             </div>
-            <div className=" h-52 mt-7 w-40 flex flex-col items-center">
+            <div className=" h-52 mt-9 w-40 flex flex-col items-center">
+                <p className="mt-10">REABILITAÇÃO</p>
                 <img src={reabili} alt="musculação" className="h-40 rounded-2xl border-orange-400 border-b-8 border-r-6 shadow-xl " />
-                <p className="w-50 text-center mt-1 ">Supere seus limites com treinos de força de alta performance</p>
+                <p className="w-60 text-center mt-1">Supere seus limites com treinos de força de alta performance</p>
             </div>
 
         </section>
