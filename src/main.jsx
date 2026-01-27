905503524fdd2568101eb90c8f45d7191ce61c67
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-01-27 15:28:04 -0300

    Finalizando deploy: esbuild e dependencias2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css' // Importa o Tailwind e estilos globais
 
 ReactDOM.createRoot(document.getElementById('root')).render(
