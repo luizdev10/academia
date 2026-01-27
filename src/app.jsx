25d225333acd72b2c814742f3378da16a00338e2
luiz henrique-IA <luizhenriquefacudade09@gmail.com>
2026-01-27 14:52:55 -0300

    deploy

--- src/app.jsx
+++ src/app.jsx
@@ -11,7 +11,7 @@ import Nutri from "./pages/Nuticao/Nutricao";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename=''>
       <div className="w-full min-h-screen bg-zinc-950">
         <NavBar />
 
